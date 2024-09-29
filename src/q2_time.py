5c1bcd0fc7fc8dd9e3f02d496945418ce27062e1
gtm144-hub <gtm144@hotmail.com>
2024-09-29 14:30:48 -0500

    UPDATE q2 functions - second version

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
+import multiprocessing as mp
+from functools import partial
 import pandas as pd
 import emoji
 
@@ -13,21 +15,23 @@ def q2_time(file_path: str) -> List[Tuple[str, int]]:
     List[Tuple[str, int]]: Top 10 days with most emojis and their emoji counts
     """
     
-    chunks = pd.read_json(file_path, lines=True, chunksize=10000)
+    chunks = pd.read_json(file_path, lines=True, chunksize=2000) # Reducimos el tamano de las particiones sin sobrepasarnos para reducir el small file problems
     
-    dfs = []
+    num_cores = mp.cpu_count()
     
-    for chunk in chunks:
-        emojis_list = chunk['content'].apply(extract_emojis).explode()
-        emojis_count = emojis_list.to_frame(name='emoji').groupby('emoji').size().reset_index(name='count')
-        dfs.append(emojis_count)
+    with mp.Pool(num_cores) as pool:
+        dfs = pool.map(process_chunk, chunks) #Procesaremos chunks en paralelo
     
     all_counts = pd.concat(dfs)
     final_counts = all_counts.groupby('emoji')['count'].sum().reset_index()
-    top_10_days = final_counts.sort_values('count', ascending=False).head(10)    
+    top_10_days = final_counts.sort_values('count', ascending=False).head(10)
     
-    return [(row['emoji'], int(row['count'])) for _, row in top_10_days.iterrows()]
+    return list(zip(top_10_days['emoji'], top_10_days['count'].astype(int)))
 
 def extract_emojis(text: str) -> list:
     """Get emojis in a given text."""
     return [x for x in text if x in emoji.EMOJI_DATA]
+
+def process_chunk(chunk: pd.DataFrame) -> pd.DataFrame:
+    emojis_list = chunk['content'].apply(extract_emojis).explode()
+    return emojis_list.to_frame(name='emoji').groupby('emoji').size().reset_index(name='count')
